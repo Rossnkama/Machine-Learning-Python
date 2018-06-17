273d7b0a4fc3facac49162b1d74a9b68cd99cb21
Ross Nkama <rossnkama@Rosss-Air.default>
2018-06-18 00:17:04 +0100

    Split data to training and test sets

--- Linear-Regression/simple_linear_regression.py
+++ Linear-Regression/simple_linear_regression.py
@@ -1,13 +1,16 @@
 # Simple Linear Regression
 
-# from sklearn.model_selection import train_test_split
-
 # Importing the libraries
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 
 # Importing the dataset
-dataset = pd.read_csv('Salary_Data.csv')
+dataset = pd.read_csv(path)
 X = dataset.iloc[:, 0]
-Y = dataset.iloc[:, 1]
+y = dataset.iloc[:, 1]
+
+# Splitting the dataset into training and test data
+# Making test size 1/3rd of the data set
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33, random_state=0)
