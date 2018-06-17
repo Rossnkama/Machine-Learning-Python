7ed744bda67b9775aabc98fd952b20ea6cb9ea84
Ross Nkama <rossnkama@Rosss-Air.default>
2018-06-17 23:32:11 +0100

    first commit

---
+++ Linear-Regression/simple_linear_regression.py
@@ -0,0 +1,10 @@
+# Simple Linear Regression
+
+# from sklearn.model_selection import train_test_split
+
+# Importing the libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Importing the dataset
